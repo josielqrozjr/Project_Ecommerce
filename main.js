832d7e10debd5a42e6c936639448dfb08120acbf
Josiel Queiroz, JR <josielqueirozjr@icloud.com>
2023-09-07 14:09:27 -0300

    Função JS do carrinho
    Alteração na função de abrir e fechar o carrinho, agora está funcionando.

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-import { inicializarCarrinho } from "./src/menuCarrinho";
+function abrirCarrinho() {
+  document.getElementById("carrinho").classList.add("carrinho-geral");
+  document.getElementById("carrinho").classList.remove("carrinho-geral-fechado");
+}
+
+function fecharCarrinho() {
+  document.getElementById("carrinho").classList.remove("carrinho-geral");
+  document.getElementById("carrinho").classList.add("carrinho-geral-fechado");
+}
+
+export function inicializarCarrinho() {
+  const botaoFecharCarrinho = document.getElementById("fechar-carrinho");
+  const botaoAbrirCarrinho = document.getElementById("abrir-carrinho");
+
+  botaoFecharCarrinho.addEventListener("click", fecharCarrinho);
+  botaoAbrirCarrinho.addEventListener("click", abrirCarrinho);
+}
 
 const catalogo = [
     {
